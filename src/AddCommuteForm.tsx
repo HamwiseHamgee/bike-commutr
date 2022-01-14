80b415c3fc38922cf8174fd9d2de064a94b7fa0f
Sam <sgphillips8@gmail.com>
2022-01-13 20:20:19 -0500

    Initial frontend commit

---
+++ src/AddCommuteForm.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import axios from "axios";
+
+const apiUrl = 'https://us-central1-bike-commutr-sam-p.cloudfunctions.net/api/commute'
+
+function AddCommuteForm() {
+  const [user, setUser] = useState("");
+  const [commute, setCommute] = useState("");
+  const [distance, setDistance] = useState("");
+  
+  return (
+    <form onSubmit={(e) => {
+        e.preventDefault(); // preventing the form from refreshing the page
+
+        // submit to our API
+        axios.post(apiUrl, {
+            user,
+            commute,
+            distance
+        })
+
+
+    }}>
+        <p>
+            <label>User:
+                <input
+                    value={user}
+                    onChange={e => setUser(e.target.value)}
+                    name="user" />
+            </label>
+        </p>
+
+        <p>
+            <label>Commute:
+                <input
+                    name="commute"
+                    value={commute}
+                    onChange={e => setCommute(e.target.value)}
+                />
+            </label>
+        </p>
+        <p>
+            <label>Distance:
+                <input
+                    type="number" name="distance"
+                    value={distance}
+                    onChange={e => setDistance(e.target.value)}
+                />
+            </label>
+        </p>
+
+        <p>
+            <button> Add Commute </button>
+        </p>
+    </form>
+)
+}
+
+export default AddCommuteForm;
